acc3edb74a93d8c550380e3f72a475121a6e2cb1
y4-kobayashi <30483328+y4-kobayashi@users.noreply.github.com>
2021-03-24 01:25:47 +0900

    map,filter

--- src/index.js
+++ src/index.js
@@ -106,3 +106,35 @@
 // // console.log(arr8);
 // arr8[0] = 100;
 // console.log(arr4);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+
+// const nameArr = ["ゆり", "りこ", "まこ"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index + 1}番目は${nameArr[index]}です`);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "ゆり") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+
+// console.log(newNameArr);
