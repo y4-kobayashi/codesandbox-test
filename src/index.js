910c91d8fd3b5a44161acc21ddce5db56aab4145
y4-kobayashi <30483328+y4-kobayashi@users.noreply.github.com>
2021-03-24 01:42:46 +0900

    論理演算子

--- src/index.js
+++ src/index.js
@@ -159,3 +159,28 @@
 //   return num1 + num2 > 100 ? `100をこえています` : `許容範囲内です`;
 // };
 // console.log(checkSum(50, 53));
+
+/**
+ * 論理演算子の本当の意味をしろう　&& ||
+ */
+
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+
+// if (flag1 && flag2) {
+//   console.log("1も2もtrueになります");
+// }
+
+// || は左側がfalseなら右側を返す
+// const num = 100;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// && は左側がtrueなら右側を返す
+// const num2 = null;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
