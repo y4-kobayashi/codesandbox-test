663950a8ff5a924f537b8f6e51af7805e89a9dbb
y4-kobayashi <30483328+y4-kobayashi@users.noreply.github.com>
2021-03-23 16:35:24 +0900

    テンプレート文字列

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/**
+ *  テンプレート文字列
+ *  */
+
+// const name = "yuri";
+// const age = 26;
+
+// // 従来
+// const message = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message);
+
+// // テンプレート文字列
+// const message2 = `私の名前は${name}です。年齢は${age}です!`;
+// console.log(message2);
