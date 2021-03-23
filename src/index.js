fb63b10390c9690b79b4eed01490f24f3f3948ca
y4-kobayashi <30483328+y4-kobayashi@users.noreply.github.com>
2021-03-23 16:42:39 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -12,3 +12,28 @@
 // // テンプレート文字列
 // const message2 = `私の名前は${name}です。年齢は${age}です!`;
 // console.log(message2);
+
+/**
+ * アロー関数
+ */
+
+// 従来
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です"));
+
+// アロー関数
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(func3(10, 20));
