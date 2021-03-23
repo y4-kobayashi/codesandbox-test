6bca24ee9a3b1090c88e82fc7e4e2e6dadd3a1ae
y4-kobayashi <30483328+y4-kobayashi@users.noreply.github.com>
2021-03-24 01:34:23 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -138,3 +138,24 @@
 // });
 
 // console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+
+// ある条件　？　条件がtrueの時：条件がfalseの時
+// const val1 = 1 > 0 ? 'true' : `false`;
+// console.log(val1);
+
+// const num = 1300;
+// // console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : `数値を入力してね`;
+
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? `100をこえています` : `許容範囲内です`;
+// };
+// console.log(checkSum(50, 53));
